48f7cd48efa870d18856d6eab312c95ca90d5a01
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-03-27 17:05:10 -0400

    FMT

--- examples/with-vpn/main.tf
+++ examples/with-vpn/main.tf
@@ -25,7 +25,7 @@ module "with-vpn" {
   attachement_subnet_ids = "${data.aws_subnet_ids.all.ids}"
   route_cidrs            = ["10.90.10.0/24"]
   vpc_id                 = "${data.aws_vpc.default.id}"
-  vpc_route_table_ids          = ["${data.aws_route_table.selected.id}"]
+  vpc_route_table_ids    = ["${data.aws_route_table.selected.id}"]
   vpc_routes_update      = false
   vpn_ips                = ["172.0.0.1"]
   vpn_types              = ["ipsec.1"]
