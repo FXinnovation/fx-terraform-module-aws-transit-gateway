9290aa75bc78714e7ad34d997859fc4cfac7709b
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-04-04 15:41:22 -0400

    test: removes deprecated variables

--- examples/with-vpn/main.tf
+++ examples/with-vpn/main.tf
@@ -20,17 +20,15 @@ data "aws_route_table" "selected" {
 module "with-vpn" {
   source = "../../"
 
-  name                              = "tftest"
-  transit_gateway_description       = "Terraform test Transit Gateway"
-  transit_gateway_subnet_ids        = "${data.aws_subnet_ids.all.ids}"
-  transit_gateway_route_cidrs_count = 1
-  transit_gateway_route_cidrs       = ["10.90.10.0/24"]
-  vpc_id                            = "${data.aws_vpc.default.id}"
-  vpc_route_table_ids               = ["${data.aws_route_table.selected.id}"]
-  vpc_routes_update                 = false
-  vpn_ips                           = ["172.0.0.1", "173.0.0.1"]
-  vpn_asns                          = [65000, 65000]
-  vpn_static_routes_options         = [false, true]
+  name                        = "tftest"
+  transit_gateway_description = "Terraform test Transit Gateway"
+  transit_gateway_subnet_ids  = "${data.aws_subnet_ids.all.ids}"
+  vpc_id                      = "${data.aws_vpc.default.id}"
+  vpc_route_table_ids         = ["${data.aws_route_table.selected.id}"]
+  vpc_routes_update           = false
+  vpn_ips                     = ["172.0.0.1", "173.0.0.1"]
+  vpn_asns                    = [65000, 65000]
+  vpn_static_routes_options   = [false, true]
 
   vpc_transit_gateway_route_count        = "1"
   vpc_transit_gateway_route_cidrs        = ["${data.aws_vpc.default.cidr_block}"]
