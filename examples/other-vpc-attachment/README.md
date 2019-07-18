5a5d054f1fdb13a9c8c0c733fa2c58b743e26f8e
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-07-18 14:45:31 -0400

    test: adds new examples for other attachment

---
+++ examples/other-vpc-attachment/README.md
@@ -0,0 +1,38 @@
+# Other VPC Attachment
+
+Use an already existing Transit Gateway and attaches it to another VPC.
+
+## Usage
+
+To run this example you need to execute:
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+Note that this example may create resources which can cost money (AWS Elastic IP, for example). Run `terraform destroy` when you don't need these resources.
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| access\_key | Credentials: AWS access key. | string | n/a | yes |
+| region | Region. | string | `"ca-central-1"` | no |
+| secret\_key | Credentials: AWS secret key. Pass this a variable, never write password in the code. | string | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| arn |  |
+| association\_default\_route\_table\_id |  |
+| id |  |
+| owner\_id |  |
+| propagation\_default\_route\_table\_id |  |
+| resource\_share\_id |  |
+| vpc\_attachment\_id |  |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ examples/other-vpc-attachment/main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  version    = "~> 2.18.0"
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+resource "random_string" "this" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+resource "random_integer" "this" {
+  min = 1
+  max = 254
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "${format("10.%s.0.0/16", random_integer.this.result)}"
+}
+
+resource "aws_subnet" "main_sub1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${format("10.%s.0.0/20", random_integer.this.result)}"
+}
+
+resource "aws_subnet" "main_sub2" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${format("10.%s.16.0/20", random_integer.this.result)}"
+}
+
+data "aws_route_table" "selected" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+module "standard" {
+  source = "../../"
+
+  prefix              = "tftest${random_string.this.result}TGW"
+  description         = "Terraform test Transit Gateway"
+  subnet_ids          = "${data.aws_subnet_ids.all.ids}"
+  vpc_id              = "${data.aws_vpc.default.id}"
+  vpc_route_table_ids = ["${data.aws_route_table.selected.id}"]
+  vpc_routes_update   = false
+
+  tags = {
+    Terraform = "tftest${random_string.this.result}TGW"
+  }
+
+  resource_share_create = false
+}
+
+module "other_vpc_attachement" {
+  source = "../../"
+
+  id         = "${module.standard.id}"
+  vpc_id     = "${aws_vpc.main.id}"
+  subnet_ids = ["${aws_subnet.main_sub1.id}", "${aws_subnet.main_sub2.id}"]
+
+  tgw_create             = false
+  vpc_attachement_create = true
+}
