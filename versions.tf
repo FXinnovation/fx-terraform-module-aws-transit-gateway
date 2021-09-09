066e95f3784f22bbbf506ba0bdfb44390f7fa788
Charles CEBE <charles.cebe@fxinnovation.com>
2021-09-09 13:10:31 -0400

    refactor: versions.tf file and providers versions

--- versions.tf
+++ versions.tf
@@ -1,9 +1,15 @@
-
 terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    aws    = ">= 3.5"
-    random = ">= 3.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
   }
 }
